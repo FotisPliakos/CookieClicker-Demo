32fa7c0ee339ce3d92fda6e312f5480dffea0d5e
FotisPliakos <48320291+FotisPliakos@users.noreply.github.com>
2022-06-20 15:12:14 +0300

    Update CookieClicker_Demo.py
    fixes

--- CookieClicker_Demo.py
+++ CookieClicker_Demo.py
@@ -10,14 +10,14 @@ driver.implicitly_wait(5)
 
 cookie = driver.find_element_by_id("bigCookie")
 cookie_count = driver.find_element_by_id("cookies")
-items = [driver.find_element_by_id("ProductPrice" + str(i)) for i in range(1,-1,-1)]
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1,-1,-1)]
 
 
 actions = ActionChains(driver)
 actions.click(cookie)
 
 for i in range(5000):
-	actions.perfom()
+	actions.perform()
 	count = int(cookie_count.text.split(" ")[0])
 	for item in items:
 		value = int(item.text)
@@ -25,4 +25,4 @@ for i in range(5000):
 			upgrade_actions = ActionChains(driver)
 			upgrade_actions.move_to_element(item)
 			upgrade_actions.click()
-			upgrade_actions.perfom()
+			upgrade_actions.perform()
