caad0ff742bd5bf41225bd211e3b899057bb3b68
FotisPliakos <48320291+FotisPliakos@users.noreply.github.com>
2022-06-20 13:47:39 +0300

    Update ClickingLanguage_CookieClicker_Demo.py

--- ClickingLanguage_CookieClicker_Demo.py
+++ ClickingLanguage_CookieClicker_Demo.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.action_chains import ActionChains
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -9,11 +8,10 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
 driver.implicitly_wait(5)
 driver.find_element_by_xpath("//div[@id='langSelect-EN']").click()
 
+
 cookie = driver.find_element_by_id("bigCookie")
 cookie_count = driver.find_element_by_id("cookies")
-items = []
-for i in range(1, -1, -1):
-	items.append(driver.find_element_by_id("productPrice" + str(i)))
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1,-1,-1)]
 
 
 actions = ActionChains(driver)
@@ -23,9 +21,9 @@ for i in range(5000):
 	actions.perform()
 	count = int(cookie_count.text.split(" ")[0])
 	for item in items:
-		value: int = int(item.text)
+		value = int(item.text)
 		if value <= count:
-			upgrade_actions: ActionChains = ActionChains(driver)
+			upgrade_actions = ActionChains(driver)
 			upgrade_actions.move_to_element(item)
 			upgrade_actions.click()
 			upgrade_actions.perform()
